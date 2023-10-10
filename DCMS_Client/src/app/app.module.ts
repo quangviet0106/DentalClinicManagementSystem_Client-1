9224ab39d641670bad257cabf70720f80ddad847
longpthe153724 <pham.danthanhlong@gmail.com>
2023-10-10 12:33:35 +0700

    2

--- DCMS_Client/src/app/app.module.ts
+++ DCMS_Client/src/app/app.module.ts
@@ -6,10 +6,25 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
-
+import { HeaderComponent } from './layouts/header/header.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { PipePipe } from './shared/pipe/pipe.pipe';
+import { AdminComponent } from './pages/admin/admin.component';
+import { ReceptionistComponent } from './pages/receptionist/receptionist.component';
+import { DoctorComponent } from './pages/doctor/doctor.component';
+import { NurseComponent } from './pages/nurse/nurse.component';
+import { PatientComponent } from './pages/patient/patient.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    PipePipe,
+    AdminComponent,
+    ReceptionistComponent,
+    DoctorComponent,
+    NurseComponent,
+    PatientComponent,
   ],
   imports: [
     BrowserModule,
